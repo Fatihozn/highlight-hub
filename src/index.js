57834a0339ab3915d5b0a296a9056490e9bef9e9
Fatihozn <100231583+Fatihozn@users.noreply.github.com>
2022-08-25 13:44:21 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const HighLight = ({ cumle, text }) => {
+  const light = (cumle, text) => {
+    text = text.toLowerCase()
+    const splitedCumle = cumle.split('')
+    const lowerCaseSplitedCumle = cumle.toLowerCase().split('')
+    let enter = true
+    const indexArray = []
+    let htmlText = ''
+
+    for (let i = 0; i < lowerCaseSplitedCumle.length; i++) {
+      enter = true
+      for (let j = 0; j < text.length; j++) {
+        if (lowerCaseSplitedCumle[i + j] !== text[j]) {
+          enter = false
+          break
+        }
+      }
+      if (enter) {
+        for (let j = 0; j < text.length; j++) {
+          indexArray.push(i + j)
+        }
+      }
+    }
+
+    for (let i = 0; i < splitedCumle.length; i++) {
+      if (indexArray.includes(i)) {
+        htmlText += `<span style="background-color: yellow">${splitedCumle[i]}</span>`
+      } else {
+        htmlText += splitedCumle[i]
+      }
+    }
+    return { __html: htmlText }
+  }
+
+  return <div dangerouslySetInnerHTML={light(cumle, text)} />
 }
